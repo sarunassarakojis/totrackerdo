4fb48c98a2351f7189c24922bb534463e01b0b3f
sarunassarakojis <sarakojis.sraunas9@gmail.com>
2016-11-30 00:37:09 +0200

    Add issue statistics fragment

---
+++ app/src/main/java/com/sarunassarakojis/totrackerdo/activity/dashboardactivity/fragment/IssueStatisticsFragment.java
@@ -0,0 +1,33 @@
+package com.sarunassarakojis.totrackerdo.activity.dashboardactivity.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.sarunassarakojis.totrackerdo.R;
+
+/**
+ * Created by Sarunas on 11/29/2016
+ */
+public class IssueStatisticsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.issue_statistics_fragment, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        TextView issueCount = (TextView) getActivity().findViewById(R.id.issue_count);
+
+        // TODO retrieve issue count from the database and set it here
+    }
+}
